e10def19b10e75ae66c04d401b442caa7dcc2541
Maciek Wójcikowski <maciek@wojcikowski.pl>
2015-02-19 14:55:59 +0100

    Add volume calculation using grid based method

--- oddt/spatial.py
+++ oddt/spatial.py
@@ -122,3 +122,51 @@ def distance_complex(x, y):
     """ Computes distance between points, similar to distance(cdist), with major difference - allows higher dimmentions of input (cdist supports 2). But it's 2-6 times slower, so use distance unless you have to nest it wit a for loop."""
     return np.sqrt(((x[...,np.newaxis,:]-y)**2).sum(axis=-1))
 
+def grid_volume(spheres, step = 0.5, box = None, inside=True):
+    """Calculate volume inside/outside of spheres using grid based aproximation
+    
+    Parameters
+    ----------
+    spheres : np.arran, shape=(n_spheres, 4)
+        An array with spheres in 3D space. Additional (last) dimention contains sphere radius.
+    step : float (default=0.5)
+        Step between grid points. NOTE: Beware of setting this too low, otherwise you will ran out of memory.
+    box : tuple or None
+        Box coordinates in 3D, tuple ((x_lower, x_upper),(y_lower, y_upper),(z_lower, z_upper)). 
+        If None, box is consturcted automatically. Box is automaticaly centered to molecule's centroid.
+    inside : bool (default=True)
+        Calculate volume inside or outside of given spheres. By default calculate volume inside spheres.
+    
+    Returns
+    -------
+    volume : float
+        Calculated volume in cubic Angstroms
+    
+    """
+    if type(box) is tuple:
+        center = spheres[:,:-1].mean(axis=0)
+        x_lower = box[0][0]
+        x_upper = box[0][1]
+        y_lower = box[1][0]
+        y_upper = box[1][1]
+        z_lower = box[2][0]
+        z_upper = box[2][1]
+        #TODO: Remove Shpheres outside the box and not "touching" it
+    else:
+        # setup box automaticaly
+        space = spheres[:,3].max() # use max radii as spacing of the grid's sides
+        x_lower = spheres[:,0].min() - space
+        x_upper = spheres[:,0].max() + space
+        y_lower = spheres[:,1].min() - space
+        y_upper = spheres[:,1].max() + space
+        z_lower = spheres[:,2].min() - space
+        z_upper = spheres[:,2].max() + space
+    
+    grid = np.mgrid[x_lower:x_upper:step,y_lower:y_upper:step,z_lower:z_upper:step,].T.reshape((-1,3))
+    #d = distance(spheres[:,:-1], grid)
+    inside_vol = (distance(spheres[:,:-1], grid) < spheres[:,3,np.newaxis]).any(axis=0).sum()*step**3
+    if inside:
+        return inside_vol
+    else:
+        return (x_upper-x_lower)*(y_upper-y_lower)*(z_upper-z_lower) - inside_vol
+
