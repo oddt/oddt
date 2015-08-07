30ae55d3b636644baf0227bccad07cdc37b72b68
Maciek Wójcikowski <maciek@wojcikowski.pl>
2015-08-07 17:33:23 +0200

    Better PDBBind

--- oddt/datasets.py
+++ oddt/datasets.py
@@ -1,26 +1,48 @@
 """ Datasets wrapped in conviniet models """
+import csv
+from os.path import isfile
 
 from oddt import toolkit
 
-class pdbbind(object):
-    def __init__(self,home, data_file = None):
-        self.home = home
-        self.ids = []
-        self.activities = []
-        if data_file:
-            with open(data_file) as f:
-                activities = {}
-                for line in f:
+# skip comments and merge multiple spaces
+def _csv_file_filter(f):
+    for row in open(f, 'rb'):
+        if row[0] == '#':
+            continue
+        yield ' '.join(row.split())
 
-                    if line[:1] == '#':
-                        continue
-                    data = line.split()
-                    self.ids.append(data[0])
-                    activities[data[0]] = float(data[3])
-            self.ids = sorted(self.ids)
-            self.activities = [activities[i] for i in self.ids]
+class pdbbind(object):
+    def __init__(self,home, version = None, default_set = None, data_file = None):
+        self.home = '%s/v%i' % (home, version) if version else home
+        self.default_set = default_set if default_set else 'general'
+        self.sets = {}
+        if version:
+            pdbind_sets = ['core', 'refined', 'general']
+            for pdbind_set in pdbind_sets:
+                if str(version) == '2007':
+                    csv_file = '%s/INDEX.%s.%s.data' % (self.home, version, pdbind_set)
+                else:
+                    csv_file = '%s/INDEX_%s_data.%s' % (self.home, pdbind_set, version)
+                if isfile(csv_file):
+                    self.sets[pdbind_set] = {}
+                    for row in csv.reader(_csv_file_filter(csv_file), delimiter=' '):
+                        pdbid = row[0]
+                        if not isfile('%s/%s/%s_pocket.pdb' % (self.home, pdbid, pdbid)):
+                            continue
+                        self.sets[pdbind_set][pdbid] = float(row[3])
+                if len(self.sets) == 0:
+                    raise Exception('There is no PDBbind sets availabe')
         else:
             pass # list directory, but no metadata then
+
+    @property
+    def ids(self):
+        return self.sets[self.default_set].keys()
+
+    @property
+    def activities(self):
+        return self.sets[self.default_set].values()
+
     def __iter__(self):
         for id in self.ids:
             yield _pdbbind_id(self.home, id)
@@ -30,7 +52,7 @@ class pdbbind(object):
             return _pdbbind_id(self.home, id)
         else:
             if type(id) is int:
-                return _pdbbind_id(self.home, self.ids[id])
+                return _pdbbind_id(self.home + '', self.ids[id])
             return None
 
 class _pdbbind_id(object):
@@ -45,4 +67,4 @@ class _pdbbind_id(object):
         return toolkit.readfile('pdb', '%s/%s/%s_pocket.pdb' % (self.home, self.id,self.id), lazy=True).next()
     @property
     def ligand(self):
-        return toolkit.readfile('sdf', '%s/%s/%s_ligand.sdf' % (self.home, self.id,self.id), lazy=True).next()
+        return toolkit.readfile('mol2', '%s/%s/%s_ligand.mol2' % (self.home, self.id,self.id), lazy=True).next()
