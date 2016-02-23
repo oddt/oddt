86cc1bd5cd7a42030916a32846e9568481ff9257
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-02-23 11:57:51 +0100

    Fix conda build #2

--- conda/deploy.sh
+++ conda/deploy.sh
@@ -3,4 +3,4 @@
 # anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
 conda skeleton pypi oddt --output-dir conda --version $TRAVIS_TAG
 conda build conda/oddt
-anaconda upload $HOME/miniconda/conda-bld/linux-64/oddt-*.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/linux-64/oddt-*.tar.bz2
