61aea8d0c1ca7002e6e43adbd907fafe3edde49d
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-02-23 14:16:40 +0100

    Final conda fix

--- conda/deploy.sh
+++ conda/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # anaconda login
 # anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
-conda skeleton pypi oddt --output-dir conda --version $TRAVIS_TAG
-conda build conda/oddt
+conda skeleton pypi oddt --output-dir conda && \
+conda build -c mwojcikowski -c rdkit conda/oddt && \
 anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/linux-64/oddt-*.tar.bz2
