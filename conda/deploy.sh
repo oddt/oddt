f9db2c20ca6a176d599d3ef3ad0337918b63df79
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-09-22 16:32:57 +0200

    Fix new conda build

--- conda/deploy.sh
+++ conda/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # anaconda login
 # anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
-conda skeleton pypi oddt --output-dir conda && \
-conda build -c mwojcikowski -c rdkit conda/oddt && \
+mkdir -p ./conda-recipe/ && cd ./conda-recipe/ && \
+conda skeleton pypi oddt && \
+conda build -c mwojcikowski -c rdkit ./oddt && \
 anaconda -t $ANACONDA_TOKEN upload $HOME/miniconda/conda-bld/linux-64/oddt-*.tar.bz2
