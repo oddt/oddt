f0eb00d95f5c077da432d0ca7544ac682c08df14
Maciek Wojcikowski <maciek@wojcikowski.pl>
2016-10-02 12:31:33 +0200

    Preliminary pandas read_sdf

--- oddt/pandas.py
+++ oddt/pandas.py
@@ -1,7 +1,27 @@
 """ Pandas extension for chemical analysis """
+from __future__ import absolute_import
 import pandas as pd
 
+import oddt
+
 pd.set_option("display.max_colwidth", 999999)
+
+def _mol_dict_reader(fmt='sdf', filepath_or_buffer=None, names=None, molecule_column='mol', append_smiles=False):
+    out = []
+    for mol in oddt.toolkit.readfile(fmt, filepath_or_buffer):
+        if names:
+            mol_data = dict((k, v) for k, v in mol.data.items() if k in names)
+        else:
+            mol_data = dict(mol.data)
+        if molecule_column:
+            mol_data[molecule_column] = mol
+        if append_smiles:
+            mol_data['smiles'] = mol.smiles
+        out.append(mol_data)
+    return ChemDataFrame(out)
+
+def read_sdf(filepath_or_buffer=None, names=None, molecule_column='mol', append_smiles=False):
+    return _mol_dict_reader(fmt='sdf', filepath_or_buffer=filepath_or_buffer, names=None, molecule_column='mol', append_smiles=False)
 
 class ChemSeries(pd.Series):
     @property
