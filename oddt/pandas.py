0ec7b05941234099c6c26fefd9403df61cbfb44b
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-10-03 13:13:22 +0200

    Create to_sdf method

--- oddt/pandas.py
+++ oddt/pandas.py
@@ -6,7 +6,12 @@ import oddt
 
 pd.set_option("display.max_colwidth", 999999)
 
-def _mol_dict_reader(fmt='sdf', filepath_or_buffer=None, names=None, molecule_column='mol', append_smiles=False):
+
+def _mol_dict_reader(fmt='sdf',
+                     filepath_or_buffer=None,
+                     names=None,
+                     molecule_column='mol',
+                     smiles_column=None):
     out = []
     for mol in oddt.toolkit.readfile(fmt, filepath_or_buffer):
         if names:
@@ -15,13 +20,22 @@ def _mol_dict_reader(fmt='sdf', filepath_or_buffer=None, names=None, molecule_co
             mol_data = dict(mol.data)
         if molecule_column:
             mol_data[molecule_column] = mol
-        if append_smiles:
-            mol_data['smiles'] = mol.smiles
+        if smiles_column:
+            mol_data[smiles_column] = mol.write('smi').split()[0]
         out.append(mol_data)
     return ChemDataFrame(out)
 
-def read_sdf(filepath_or_buffer=None, names=None, molecule_column='mol', append_smiles=False):
-    return _mol_dict_reader(fmt='sdf', filepath_or_buffer=filepath_or_buffer, names=None, molecule_column='mol', append_smiles=False)
+
+def read_sdf(filepath_or_buffer=None,
+             names=None,
+             molecule_column='mol',
+             smiles_column=None):
+    return _mol_dict_reader(fmt='sdf',
+                            filepath_or_buffer=filepath_or_buffer,
+                            names=names,
+                            molecule_column=molecule_column,
+                            smiles_column=smiles_column)
+
 
 class ChemSeries(pd.Series):
     @property
@@ -35,6 +49,25 @@ class ChemDataFrame(pd.DataFrame):
     Note:
     Thanks to: http://blog.snapdragon.cc/2015/05/05/subclass-pandas-dataframe-to-save-custom-attributes/
     """
+    def to_sdf(self,
+               filepath_or_buffer=None,
+               update_properties=True,
+               molecule_column='mol',
+               names=None):
+        out = oddt.toolkit.Outputfile('sdf', filepath_or_buffer, overwrite=True)
+        for ix, row in self.iterrows():
+            mol = row[molecule_column].clone
+            if update_properties:
+                new_data = row.to_dict()
+                del new_data[molecule_column]
+                mol.data.update(new_data)
+            if names:
+                for k in mol.data.keys():
+                    if k not in names:
+                        del mol.data[k]
+            out.write(mol)
+        out.close()
+
     def to_html(self, *args, **kwargs):
         kwargs['escape'] = False
         return super(ChemDataFrame, self).to_html(*args, **kwargs)
