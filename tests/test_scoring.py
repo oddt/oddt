5142af828ce11dc553a5d6b64f68746b96e041dc
Maciek Wójcikowski <maciek@wojcikowski.pl>
2017-01-09 16:45:25 +0100

    Cleanup and help debug

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -9,19 +9,19 @@ from sklearn.utils.testing import (assert_true,
                                    assert_array_almost_equal)
 
 import oddt
-from oddt.scoring.functions import rfscore
+from oddt.scoring.functions import rfscore, nnscore
 
 test_data_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_rfscore():
+    """Test RFScore v1-3 descriptors generators"""
     mols = list(oddt.toolkit.readfile('sdf', os.path.join(test_data_dir, 'data/dude/xiap/actives_docked.sdf')))
     rec = next(oddt.toolkit.readfile('pdb', os.path.join(test_data_dir, 'data/dude/xiap/receptor_rdkit.pdb')))
 
     # Delete molecule which has differences in Acceptor-Donor def in RDK and OB
     del mols[65]
 
-    # test descriptors
     for v in [1, 2, 3]:
         descs = rfscore(version=v, protein=rec).descriptor_generator.build(mols)
         # save correct results (for future use)
@@ -31,4 +31,11 @@ def test_rfscore():
         #            fmt='%.16g',
         #            delimiter=',')
         descs_correct = np.loadtxt(os.path.join(test_data_dir, 'data/results/xiap/rfscore_v%i_descs.csv' % v), delimiter=',')
+
+        # help debug erroes
+        for i in range(descs.shape[1]):
+            mask = np.abs(descs[:, i] - descs_correct[:, i]) > 1e-4
+            if mask.sum() > 1:
+                print(i, np.vstack((descs[mask, i], descs_correct[mask, i])))
+
         assert_array_almost_equal(descs, descs_correct, decimal=4)
