af9ab8740cfc8a08a235b88eaaa20bb7702ece9b
Maciek Wójcikowski <maciek@wojcikowski.pl>
2017-01-19 14:34:16 +0100

    Cleanup of classifiers

--- oddt/scoring/models/classifiers.py
+++ oddt/scoring/models/classifiers.py
@@ -8,15 +8,17 @@ from sklearn.neural_network import MLPClassifier
 
 __all__ = ['randomforest', 'svm', 'neuralnetwork']
 
+
 class neuralnetwork(ClassifierMixin):
     def __init__(self, *args, **kwargs):
         """ Assemble Neural network using sklearn pipeline """
         # Cherrypick arguments for model. Exclude 'steps', which is pipeline argument
-        local_kwargs = {key: kwargs.pop(key) for key in list(kwargs.keys()) if key != 'steps' and len(key.split('__', 1)) == 1}
+        local_kwargs = {key: kwargs.pop(key) for key in list(kwargs.keys())
+                        if key != 'steps' and len(key.split('__', 1)) == 1}
         self.pipeline = Pipeline([('empty_dims_remover', VarianceThreshold()),
                                   ('scaler', StandardScaler()),
                                   ('neural_network', MLPClassifier(*args, **local_kwargs))
-                                 ]).set_params(**kwargs)
+                                  ]).set_params(**kwargs)
 
     def get_params(self, deep=True):
         return self.pipeline.get_params(deep=deep)
@@ -31,18 +33,26 @@ class neuralnetwork(ClassifierMixin):
     def predict(self, descs):
         return self.pipeline.predict(descs)
 
+    def predict_proba(self, descs):
+        return self.pipeline.predict_proba(descs)
+
+    def predict_log_proba(self, descs):
+        return self.pipeline.predict_log_proba(descs)
+
     def score(self, descs, target_values):
         return self.pipeline.score(descs, target_values)
+
 
 class svm(ClassifierMixin):
     def __init__(self, *args, **kwargs):
         """ Assemble a proper SVM classifier"""
         # Cherrypick arguments for model. Exclude 'steps', which is pipeline argument
-        local_kwargs = {key: kwargs.pop(key) for key in list(kwargs.keys()) if key != 'steps' and len(key.split('__', 1)) == 1}
+        local_kwargs = {key: kwargs.pop(key) for key in list(kwargs.keys())
+                        if key != 'steps' and len(key.split('__', 1)) == 1}
         self.pipeline = Pipeline([('empty_dims_remover', VarianceThreshold()),
                                   ('scaler', StandardScaler()),
                                   ('svm', SVC(*args, **local_kwargs))
-                                 ]).set_params(**kwargs)
+                                  ]).set_params(**kwargs)
 
     def get_params(self, deep=True):
         return self.pipeline.get_params(deep=deep)
@@ -56,6 +66,12 @@ class svm(ClassifierMixin):
 
     def predict(self, descs):
         return self.pipeline.predict(descs)
+
+    def predict_proba(self, descs):
+        return self.pipeline.predict_proba(descs)
+
+    def predict_log_proba(self, descs):
+        return self.pipeline.predict_log_proba(descs)
 
     def score(self, descs, target_values):
         return self.pipeline.score(descs, target_values)
