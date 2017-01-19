d13d51bf1af603395123135665e657760873c52f
Maciek Wójcikowski <maciek@wojcikowski.pl>
2017-01-19 14:35:26 +0100

    Cleanup of regressors

--- oddt/scoring/models/regressors.py
+++ oddt/scoring/models/regressors.py
@@ -15,15 +15,17 @@ except ImportError:
 
 __all__ = ['randomforest', 'svm', 'pls', 'neuralnetwork', 'mlr']
 
+
 class neuralnetwork(RegressorMixin):
     def __init__(self, *args, **kwargs):
         """ Assemble Neural network using sklearn pipeline """
         # Cherrypick arguments for model. Exclude 'steps', which is pipeline argument
-        local_kwargs = {key: kwargs.pop(key) for key in list(kwargs.keys()) if key != 'steps' and len(key.split('__', 1)) == 1}
+        local_kwargs = {key: kwargs.pop(key) for key in list(kwargs.keys())
+                        if key != 'steps' and len(key.split('__', 1)) == 1}
         self.pipeline = Pipeline([('empty_dims_remover', VarianceThreshold()),
                                   ('scaler', StandardScaler()),
                                   ('neural_network', MLPRegressor(*args, **local_kwargs))
-                                 ]).set_params(**kwargs)
+                                  ]).set_params(**kwargs)
 
     def get_params(self, deep=True):
         return self.pipeline.get_params(deep=deep)
@@ -41,15 +43,17 @@ class neuralnetwork(RegressorMixin):
     def score(self, descs, target_values):
         return self.pipeline.score(descs, target_values)
 
+
 class svm(RegressorMixin):
     def __init__(self, *args, **kwargs):
         """ Assemble a proper SVM using sklearn tools regressor """
         # Cherrypick arguments for model. Exclude 'steps', which is pipeline argument
-        local_kwargs = {key: kwargs.pop(key) for key in list(kwargs.keys()) if key != 'steps' and len(key.split('__', 1)) == 1}
+        local_kwargs = {key: kwargs.pop(key) for key in list(kwargs.keys())
+                        if key != 'steps' and len(key.split('__', 1)) == 1}
         self.pipeline = Pipeline([('empty_dims_remover', VarianceThreshold()),
                                   ('scaler', StandardScaler()),
                                   ('svm', SVR(*args, **local_kwargs))
-                                 ]).set_params(**kwargs)
+                                  ]).set_params(**kwargs)
 
     def get_params(self, deep=True):
         return self.pipeline.get_params(deep=deep)
