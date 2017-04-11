e70ef894dbb5a7ed9c4a55aad37aec9bd07a5edd
kinga322 <kinga@koziel.org.pl>
2017-04-11 13:39:56 +0200

    Add shape.py with functions for USR, USRCAT and Electroshape shape desciptors

---
+++ oddt/shape.py
@@ -0,0 +1,258 @@
+import numpy as np
+from numpy.linalg import norm
+from scipy.stats import moment
+from scipy.special import cbrt
+
+
+def common_usr(molecule, ctd=None, cst=None, fct=None, ftf=None, atoms_type=None):
+    """Function used in USR and USRCAT function
+
+        Parameters
+        ----------
+        molecule : oddt.toolkit.Molecule
+            Molecule to compute USR shape descriptor
+
+        ctd : numpy array or None (default = None)
+            Coordinates of the molecular centroid
+            If 'None', the point is calculated
+
+        cst : numpy array or None (default = None)
+            Coordinates of the closest atom to the molecular centroid
+            If 'None', the point is calculated
+
+        fct : numpy array or None (default = None)
+            Coordinates of the farthest atom to the molecular centroid
+            If 'None', the point is calculated
+
+        ftf : numpy array or None (default = None)
+            Coordinates of the farthest atom
+            to the farthest atom to the molecular centroid
+            If 'None', the point is calculated
+
+        atoms_type : str or None (default None)
+            Type of atoms to be selected from atom_dict
+            If 'None', all atoms are used to calculate shape descriptor
+
+        Returns
+        -------
+        shape_descriptor : numpy array, shape = (12)
+            Array describing shape of molecule
+    """
+    if atoms_type is None:
+        atoms = molecule.atom_dict['coords']
+    else:
+        if atoms_type == 'ishydrophobe':
+            mask = (molecule.atom_dict['ishalogen'] |
+                    molecule.atom_dict['ishydrophobe'] |
+                    (molecule.atom_dict['atomicnum'] == 16))
+        else:
+            mask = molecule.atom_dict[atoms_type]
+        atoms = molecule.atom_dict[mask]['coords']
+
+    if len(atoms) == 0:
+        return np.zeros(12), ((0., 0., 0.),) * 4
+
+    if ctd is None:
+        ctd = atoms.mean(0)
+    distances_ctd = norm(atoms - ctd, axis=1)
+
+    if cst is None:
+        cst = atoms[distances_ctd.argmin()]
+    distances_cst = norm(atoms - cst, axis=1)
+
+    if fct is None:
+        fct = atoms[distances_ctd.argmax()]
+    distances_fct = norm(atoms - fct, axis=1)
+
+    if ftf is None:
+        ftf = atoms[distances_fct.argmax()]
+    distances_ftf = norm(atoms - ftf, axis=1)
+
+    distances_list = [distances_ctd, distances_cst, distances_fct, distances_ftf]
+
+    shape_descriptor = np.zeros(12)
+
+    for i, distances in enumerate(distances_list):
+        shape_descriptor[i * 3 + 0] = np.mean(distances)
+        shape_descriptor[i * 3 + 1] = np.var(distances)
+        shape_descriptor[i * 3 + 2] = moment(distances, moment=3)
+
+    return shape_descriptor, (ctd, cst, fct, ftf)
+
+
+def usr(molecule):
+    """Computes USR shape descriptor based on
+        Ballester PJ, Richards WG (2007). Ultrafast shape recognition to search
+        compound databases for similar molecular shapes. Journal of
+        computational chemistry, 28(10):1711-23.
+        http://dx.doi.org/10.1002/jcc.20681
+
+        Parameters
+        ----------
+        molecule : oddt.toolkit.Molecule
+            Molecule to compute USR shape descriptor
+
+        Returns
+        -------
+        shape_descriptor : numpy array, shape = (12)
+            Array describing shape of molecule
+    """
+    return common_usr(molecule)[0]
+
+
+def usr_cat(molecule):
+    """Computes USRCAT shape descriptor based on
+        Adrian M Schreyer, Tom Blundell (2012). USRCAT: real-time ultrafast
+        shape recognition with pharmacophoric constraints. Journal of
+        Cheminformatics, 2012 4:27.
+        http://dx.doi.org/10.1186/1758-2946-4-27
+
+        Parameters
+        ----------
+        molecule : oddt.toolkit.Molecule
+            Molecule to compute USRCAT shape descriptor
+
+        Returns
+        -------
+        shape_descriptor : numpy array, shape = (60)
+            Array describing shape of molecule
+    """
+    all_atoms_shape, points = common_usr(molecule)
+    ctd, cst, fct, ftf = points
+    hydrophobic_shape = common_usr(
+        molecule, ctd, cst, fct, ftf, 'ishydrophobe')[0]
+    aromatic_shape = common_usr(molecule, ctd, cst, fct, ftf, 'isaromatic')[0]
+    acceptor_shape = common_usr(molecule, ctd, cst, fct, ftf, 'isacceptor')[0]
+    donor_shape = common_usr(molecule, ctd, cst, fct, ftf, 'isdonor')[0]
+
+    cat_shape = np.hstack((all_atoms_shape, hydrophobic_shape,
+                           aromatic_shape, acceptor_shape, donor_shape))
+
+    return np.nan_to_num(cat_shape)
+
+
+def electroshape(mol):
+    """Computes shape descriptor based on
+    Armstrong, M. S. et al. ElectroShape: fast molecular similarity
+    calculations incorporating shape, chirality and electrostatics.
+    J Comput Aided Mol Des 24, 789-801 (2010).
+    http://dx.doi.org/doi:10.1007/s10822-010-9374-0
+
+    Aside from spatial coordinates, atoms' charges are also used
+    as the fourth dimension to describe shape of the molecule.
+
+        Parameters
+        ----------
+            mol : oddt.toolkit.Molecule
+
+        Returns
+        -------
+            shape_descriptor : numpy array, shape = (15)
+                               Array describing shape of molecule
+    """
+    if (mol.atom_dict['coords'] == 0).all():
+        raise Exception('Molecule needs 3D coordinates')
+
+    mi = 25  # scaling factor converting electron charges to Angstroms
+
+    four_dimensions = np.column_stack((mol.atom_dict['coords'],
+                                       mol.atom_dict['charge'] * mi))
+
+    c1 = four_dimensions.mean(0)  # geometric centre of the molecule
+    distances_c1 = norm(four_dimensions - c1, axis=1)
+
+    c2 = four_dimensions[distances_c1.argmax()]  # atom position furthest from c1
+    distances_c2 = norm(four_dimensions - c2, axis=1)
+
+    c3 = four_dimensions[distances_c2.argmax()]  # atom position furthest from c2
+    distances_c3 = norm(four_dimensions - c3, axis=1)
+
+    vector_a = c2 - c1
+    vector_b = c3 - c1
+    vector_as = vector_a[:3]    # spatial parts of these vectors -
+    vector_bs = vector_b[:3]    # the first three coordinates
+    vector_c = ((norm(vector_a) /
+                (2 * norm(np.cross(vector_as, vector_bs))))
+                * np.cross(vector_as, vector_bs))
+
+    vector_c1s = c1[:3]
+
+    max_charge = np.array(np.amax(mol.atom_dict['charge']) * mi)
+    min_charge = np.array(np.amin(mol.atom_dict['charge']) * mi)
+
+    c4 = np.append(vector_c1s + vector_c, max_charge)
+    c5 = np.append(vector_c1s + vector_c, min_charge)
+
+    distances_c4 = norm(four_dimensions - c4, axis=1)
+    distances_c5 = norm(four_dimensions - c5, axis=1)
+
+    distances_list = [distances_c1, distances_c2, distances_c3,
+                      distances_c4, distances_c5]
+
+    shape_descriptor = np.zeros(15)
+
+    i = 0
+    for distances in distances_list:
+        mean = np.mean(distances)
+        shape_descriptor[0 + i] = mean
+        shape_descriptor[1 + i] = np.std(distances)
+        shape_descriptor[2 + i] = cbrt(np.sum(((distances - mean) ** 3) / distances.size))
+        i += 3
+
+    return shape_descriptor
+
+
+def usr_similarity(mol1_shape, mol2_shape, ow=1., hw=1., rw=1., aw=1., dw=1.):
+    """Computes similarity between molecules
+
+        Parameters
+        ----------
+        mol1_shape : numpy array
+            USR shape descriptor
+
+        mol2_shape : numpy array
+            USR shape descriptor
+
+        ow : float (default = 1.)
+            Scaling factor for all atoms
+            Only used for USRCAT, ignored for other types
+
+        hw : float (default = 1.)
+            Scaling factor for hydrophobic atoms
+            Only used for USRCAT, ignored for other types
+
+        rw : float (default = 1.)
+            Scaling factor for aromatic atoms
+            Only used for USRCAT, ignored for other types
+
+        aw : float (default = 1.)
+            Scaling factor for acceptors
+            Only used for USRCAT, ignored for other types
+
+        dw : float (default = 1.)
+            Scaling factor for donors
+            Only used for USRCAT, ignored for other types
+
+        Returns
+        -------
+        similarity : float from 0 to 1
+            Similarity between shapes of molecules,
+            1 indicates identical molecules
+    """
+    if mol1_shape.shape[0] == 12 and mol2_shape.shape[0] == 12:
+        sim = 1. / (1. + (1. / 12) * np.sum(np.fabs(mol1_shape - mol2_shape)))
+    elif mol1_shape.shape[0] == 60 and mol2_shape.shape[0] == 60:
+        sim = 1. / (1 +
+                    ow * (1. / 12) * np.sum(np.abs(mol1_shape[0:12] - mol2_shape[0:12])) +
+                    hw * (1. / 12) * np.sum(np.abs(mol1_shape[12:24] - mol2_shape[12:24])) +
+                    rw * (1. / 12) * np.sum(np.abs(mol1_shape[24:36] - mol2_shape[24:36])) +
+                    aw * (1. / 12) * np.sum(np.abs(mol1_shape[36:48] - mol2_shape[36:48])) +
+                    dw * (1. / 12) * np.sum(np.abs(mol1_shape[48:] - mol2_shape[48:])))
+    elif mol1_shape.shape[0] == 15 and mol2_shape.shape[0] == 15:
+        sim = 1. / (1 + (1. / 15) * np.sum(np.fabs(mol1_shape - mol2_shape)))
+    else:
+        raise Exception('Given vectors are not valid USR shape descriptors '
+                        'or come from different methods. Correct vector lengths'
+                        'are: 12 for USR, 60 for USRCAT, 15 for Electroshape')
+
+    return sim
---
+++ tests/test_shape.py
@@ -0,0 +1,166 @@
+import os
+import numpy as np
+from sklearn.utils.testing import (assert_array_equal,
+                                   assert_array_almost_equal,
+                                   assert_almost_equal)
+import oddt
+from oddt.shape import usr, usr_cat, electroshape, usr_similarity
+
+test_data_dir = os.path.dirname(os.path.abspath(__file__))
+benzene_sdf = """
+     RDKit          3D
+
+  6  6  0  0  0  0  0  0  0  0999 V2000
+   -1.0971    0.8220    0.2458 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.1708    1.3788    0.0966 C   0  0  0  0  0  0  0  0  0  0  0  0
+    1.2678    0.5568   -0.1492 C   0  0  0  0  0  0  0  0  0  0  0  0
+    1.0971   -0.8220   -0.2458 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.1708   -1.3788   -0.0966 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -1.2678   -0.5568    0.1491 C   0  0  0  0  0  0  0  0  0  0  0  0
+  1  2  2  0
+  2  3  1  0
+  3  4  2  0
+  4  5  1  0
+  5  6  2  0
+  6  1  1  0
+M  END
+
+"""
+methylo_sdf = """
+     RDKit          3D
+
+  7  7  0  0  0  0  0  0  0  0999 V2000
+    0.2879   -1.1465    0.0728 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -1.0997   -1.1111    0.1887 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -1.7974    0.0457   -0.1502 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -1.1081    1.1672   -0.6050 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.2795    1.1321   -0.7210 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.9778   -0.0249   -0.3821 C   0  0  0  0  0  0  0  0  0  0  0  0
+    2.4600   -0.0625   -0.5059 C   0  0  0  0  0  0  0  0  0  0  0  0
+  1  2  2  0
+  2  3  1  0
+  3  4  2  0
+  4  5  1  0
+  5  6  2  0
+  6  7  1  0
+  6  1  1  0
+M  END
+
+"""
+benzene = oddt.toolkit.readstring('sdf', benzene_sdf)
+methylo = oddt.toolkit.readstring('sdf', methylo_sdf)
+
+
+def test_usr():
+    """USR test"""
+    benzene_usr = np.array((1.392709, 0., 0.,
+                            1.732536, 0.877535, -0.580651,
+                            1.732576, 0.877555, -0.580693,
+                            1.732573, 0.877552, -0.580686))
+    methylo_usr = np.array((1.57291, 0.20536, 0.064638,
+                            1.697949, 0.759765, -0.420457,
+                            2.614764, 1.94016, -1.660038,
+                            2.095664, 1.54289, 0.117823))
+
+    assert_array_almost_equal(usr(benzene), benzene_usr)
+    assert_array_almost_equal(usr(methylo), methylo_usr)
+
+
+def test_usr_cat():
+    """USRCAT test"""
+    benzene_usrcat = np.array((1.392709, 0., 0.,
+                               1.732536, 0.877535, -0.580651,
+                               1.732576, 0.877555, -0.580693,
+                               1.732573, 0.877552, -0.580686,
+                               1.392709, 0., 0.,
+                               1.732536, 0.877535, -0.580651,
+                               1.732576, 0.877555, -0.580693,
+                               1.732573, 0.877552, -0.580686,
+                               1.392709, 0., 0.,
+                               1.732536, 0.877535, -0.580651,
+                               1.732576, 0.877555, -0.580693,
+                               1.732573, 0.877552, -0.580686,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.))
+    methylo_usrcat = np.array((1.57291, 0.20536, 0.064638,
+                               1.697949, 0.759765, -0.420457,
+                               2.614764, 1.94016, -1.660038,
+                               2.095664, 1.54289, 0.117823,
+                               1.57291, 0.20536, 0.064638,
+                               1.697949, 0.759765, -0.420457,
+                               2.614764, 1.94016, -1.660038,
+                               2.095664, 1.54289, 0.117823,
+                               1.423502, 0.083326, -0.003973,
+                               1.732968, 0.877809, -0.58125,
+                               3.050558, 0.934105, -0.26119,
+                               1.732674, 0.877707, -0.58073,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.,
+                               0., 0., 0.))
+
+    assert_array_almost_equal(usr_cat(benzene), benzene_usrcat)
+    assert_array_almost_equal(usr_cat(methylo), methylo_usrcat)
+
+
+def test_electroshape():
+    """Electroshape test"""
+    if oddt.toolkit.backend == 'rdk':
+        benzene_usr = np.array((1.392709, 0.000024, 0.000021,
+                                1.732576, 0.936779, -0.834287,
+                                1.732573, 0.936777, -0.834284,
+                                1.482042, 0.477624, -0.293458,
+                                1.482042, 0.477624, -0.293458))
+        methylo_usr = np.array((1.58227, 0.461717, 0.417984,
+                                2.649053, 1.404615, -1.20629,
+                                2.103215, 1.252546, 0.544395,
+                                2.009275, 0.651085, -0.488366,
+                                1.955503, 0.693917, -0.521708))
+    else:
+        benzene_usr = np.array((1.39270878, 0.000023678, 0.0000208289,
+                                1.73257649, 0.936778843, -0.834286869,
+                                1.73257256, 0.936777234, -0.834283948,
+                                1.48204167, 0.477623908, -0.293458485,
+                                1.48204167, 0.477623908, -0.293458485))
+        methylo_usr = np.array((1.61146379, 0.46305567, 0.46938854,
+                                2.76274943, 1.37034833, -1.2889241,
+                                2.11388564, 1.2641263, 0.57394201,
+                                2.18083588, 0.57593416, -0.48947371,
+                                2.06548012, 0.71989938, -0.44118673))
+
+    assert_array_almost_equal(electroshape(benzene), benzene_usr)
+    assert_array_almost_equal(electroshape(methylo), methylo_usr)
+
+
+def test_usr_similarity():
+    """Similarity function for USR test"""
+    similarity_usr = 0.68517293875665453
+    assert_almost_equal(usr_similarity(usr(benzene), usr(methylo)), similarity_usr)
+
+
+def test_usrcat_similarity():
+    """Similarity function for USRCAT test"""
+    similarity_usrcat = 0.48306484140373468
+    assert_almost_equal(usr_similarity(usr_cat(benzene), usr_cat(methylo)),
+                 similarity_usrcat)
+
+
+def test_elecroshape_similarity():
+    """Similarity function for Electroshape test"""
+    if oddt.toolkit.backend == 'rdk':
+        similarity_electroshape = 0.69110953340712389
+    else:
+        similarity_electroshape = 0.67710967360721197
+
+    assert_almost_equal(usr_similarity(electroshape(benzene), electroshape(methylo)),
+                 similarity_electroshape)
