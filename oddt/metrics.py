f6e2334e5051095d93140067d8938c5d6c325477
marta-sd <martasd@ibb.waw.pl>
2017-11-27 14:56:16 +0100

    tests and cleanup for metrics

--- oddt/metrics.py
+++ oddt/metrics.py
@@ -6,7 +6,9 @@ import numpy as np
 from scipy.stats import linregress
 from sklearn.metrics import roc_curve as roc, auc, mean_squared_error
 
-__all__ = ['roc', 'auc', 'roc_auc', 'roc_log_auc', 'enrichment_factor', 'random_roc_log_auc', 'rmse']
+__all__ = ['roc', 'auc', 'roc_auc', 'roc_log_auc', 'enrichment_factor',
+           'random_roc_log_auc', 'rmse']
+
 
 def roc_auc(y_true, y_score, pos_label=None, ascending_score=True):
     """Computes ROC AUC score
@@ -14,7 +16,8 @@ def roc_auc(y_true, y_score, pos_label=None, ascending_score=True):
     Parameters
     ----------
         y_true : array, shape=[n_samples]
-            True binary labels, in range {0,1} or {-1,1}. If positive label is different than 1, it must be explicitly defined.
+            True binary labels, in range {0,1} or {-1,1}. If positive label is
+            different than 1, it must be explicitly defined.
 
         y_score : array, shape=[n_samples]
             Scores for tested series of samples
@@ -23,7 +26,9 @@ def roc_auc(y_true, y_score, pos_label=None, ascending_score=True):
             Positive label of samples (if other than 1)
 
         ascending_score: bool (default=True)
-            Indicates if your score is ascendig. Ascending score icreases with deacreasing activity. In other words it ascends on ranking list (where actives are on top).
+            Indicates if your score is ascendig. Ascending score icreases with
+            deacreasing activity. In other words it ascends on ranking list
+            (where actives are on top).
 
     Returns
     -------
@@ -34,6 +39,7 @@ def roc_auc(y_true, y_score, pos_label=None, ascending_score=True):
         y_score = -y_score
     fpr, tpr, tresholds = roc(y_true, y_score, pos_label=pos_label)
     return auc(fpr, tpr, reorder=False)
+
 
 def rmse(y_true, y_pred):
     """Compute Root Mean Squared Error (RMSE)
@@ -53,13 +59,17 @@ def rmse(y_true, y_pred):
     """
     return np.sqrt(mean_squared_error(y_true, y_pred))
 
+
 def enrichment_factor(y_true, y_score, percentage=1, pos_label=None, kind='fold'):
-    """Computes enrichment factor for given percentage, i.e. EF_1% is enrichment factor for first percent of given samples.
+    """Computes enrichment factor for given percentage, i.e. EF_1% is
+    enrichment factor for first percent of given samples. This function assumes
+    that results are already sorted and samples with best predictions are first.
 
     Parameters
     ----------
         y_true : array, shape=[n_samples]
-            True binary labels, in range {0,1} or {-1,1}. If positive label is different than 1, it must be explicitly defined.
+            True binary labels, in range {0,1} or {-1,1}. If positive label is
+            different than 1, it must be explicitly defined.
 
         y_score : array, shape=[n_samples]
             Scores for tested series of samples
@@ -72,8 +82,9 @@ def enrichment_factor(y_true, y_score, percentage=1, pos_label=None, kind='fold'
 
         kind: 'fold' or 'percentage' (default='fold')
             Two kinds of enrichment factor: fold and percentage.
-            Fold shows the increase over random distribution (1 is random, the higher EF the better enrichment).
-            Percentage returns the fraction of positive labels within the top x% of dataset.
+            Fold shows the increase over random distribution (1 is random, the
+            higher EF the better enrichment). Percentage returns the fraction of
+            positive labels within the top x% of dataset.
 
     Returns
     -------
@@ -86,19 +97,22 @@ def enrichment_factor(y_true, y_score, percentage=1, pos_label=None, kind='fold'
     assert labels.sum() > 0, "There are no correct predicions. Double-check the pos_label"
     assert len(labels) > 0, "Sample size must be greater than 0"
     # calculate fraction of positve labels
-    n_perc = int(ceil(float(percentage)/100.*len(labels)))
-    out = float(labels[:n_perc].sum())/n_perc
+    n_perc = int(ceil(float(percentage) / 100. * len(labels)))
+    out = float(labels[:n_perc].sum()) / n_perc
     if kind == 'fold':
-         out /= (float(labels.sum())/len(labels))
+        out /= (float(labels.sum()) / len(labels))
     return out
 
-def roc_log_auc(y_true, y_score, pos_label=None, ascending_score=True, log_min=0.001, log_max=1.):
-    """Computes area under semi-log ROC for random distribution.
+
+def roc_log_auc(y_true, y_score, pos_label=None, ascending_score=True,
+                log_min=0.001, log_max=1.):
+    """Computes area under semi-log ROC.
 
     Parameters
     ----------
         y_true : array, shape=[n_samples]
-            True binary labels, in range {0,1} or {-1,1}. If positive label is different than 1, it must be explicitly defined.
+            True binary labels, in range {0,1} or {-1,1}. If positive label is
+            different than 1, it must be explicitly defined.
 
         y_score : array, shape=[n_samples]
             Scores for tested series of samples
@@ -107,13 +121,16 @@ def roc_log_auc(y_true, y_score, pos_label=None, ascending_score=True, log_min=0
             Positive label of samples (if other than 1)
 
         ascending_score: bool (default=True)
-            Indicates if your score is ascendig. Ascending score icreases with deacreasing activity. In other words it ascends on ranking list (where actives are on top).
+            Indicates if your score is ascendig. Ascending score icreases with
+            deacreasing activity. In other words it ascends on ranking list
+            (where actives are on top).
 
         log_min : float (default=0.001)
-            Minimum logarithm value for estimating AUC
+            Minimum value for estimating AUC. Lower values will be clipped for
+            numerical stability.
 
         log_max : float (default=1.)
-            Maximum logarithm value for estimating AUC.
+            Maximum value for estimating AUC. Higher values will be ignored.
 
     Returns
     -------
@@ -123,9 +140,11 @@ def roc_log_auc(y_true, y_score, pos_label=None, ascending_score=True, log_min=0
     if ascending_score:
         y_score = -y_score
     fpr, tpr, t = roc(y_true, y_score, pos_label=pos_label)
-    idx = (fpr >= log_min) & (fpr <= log_max)
-    log_fpr = 1-np.log10(fpr[idx])/np.log10(log_min)
+    fpr = fpr.clip(log_min)
+    idx = (fpr <= log_max)
+    log_fpr = 1 - np.log10(fpr[idx]) / np.log10(log_min)
     return auc(log_fpr, tpr[idx], reorder=False)
+
 
 def random_roc_log_auc(log_min=0.001, log_max=1.):
     """Computes area under semi-log ROC for random distribution.
@@ -143,8 +162,7 @@ def random_roc_log_auc(log_min=0.001, log_max=1.):
         auc : float
             semi-log ROC AUC for random distribution
     """
-    return (log_max-log_min)/(np.log(10)*np.log10(log_max/log_min))
-
+    return (log_max - log_min) / (np.log(10) * np.log10(log_max / log_min))
 
 
 def standard_deviation_error(y_true, y_pred):
---
+++ tests/test_metrics.py
@@ -0,0 +1,67 @@
+from nose.tools import (assert_less_equal, assert_less, assert_greater_equal,
+                        assert_greater,
+                        assert_equal)
+
+import numpy as np
+np.random.seed(42)
+
+from oddt.metrics import (roc_auc, roc_log_auc, random_roc_log_auc,
+                          enrichment_factor,
+                          rmse, standard_deviation_error)
+
+
+classes = np.array([0] * 90000 + [1] * 10000)
+# poorly separated
+poor_classes = np.random.rand(100000) * 100
+
+# well separated
+good_classes = np.concatenate([np.random.rand(90000) * 10 + 100,
+                               np.random.rand(10000) * 10 + 1000])
+
+values = np.arange(100000)
+poor_values = np.random.rand(100000) * 100    # poorly predicted
+good_values = np.arange(100000) + np.random.rand(100000)  # correctly predicted
+
+
+def test_roc_auc():
+    score = roc_auc(classes, poor_classes)
+    assert_less_equal(score, 0.55)
+    assert_greater_equal(score, 0.45)
+
+    assert_equal(roc_auc(classes, good_classes, ascending_score=True), 0.0)
+    assert_equal(roc_auc(classes, good_classes, ascending_score=False), 1.0)
+
+
+def test_roc_log_auc():
+    random_score = random_roc_log_auc()
+    score = roc_log_auc(classes, poor_classes)
+    assert_less(np.abs(score - random_score), 0.01)
+
+    assert_equal(roc_log_auc(classes, good_classes, ascending_score=True), 0)
+    assert_equal(roc_log_auc(classes, good_classes, ascending_score=False), 1)
+
+
+def test_enrichment():
+    order = sorted(range(len(poor_classes)), key=lambda k: poor_classes[k],
+                   reverse=True)
+    ef = enrichment_factor(classes[order], poor_classes[order])
+    assert_less_equal(ef, 1.5)
+
+    order = sorted(range(len(good_classes)), key=lambda k: good_classes[k],
+                   reverse=True)
+    ef = enrichment_factor(classes[order], good_classes[order])
+    assert_equal(ef, 10)
+
+    ef = enrichment_factor(classes[order], good_classes[order],
+                           kind='percentage')
+    assert_equal(ef, 1)
+
+
+def test_rmse():
+    assert_greater_equal(rmse(values, poor_values), 30)
+    assert_less_equal(rmse(values, good_values), 1)
+
+
+def test_standard_deviation_error():
+    assert_less(standard_deviation_error(values, good_values), 1.1)
+    assert_greater(standard_deviation_error(values, poor_values), 5e4)
