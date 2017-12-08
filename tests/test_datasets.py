cb5c96198ff75e2b96cfd166a5ac336c1ab9357d
marta-sd <martasd@ibb.waw.pl>
2017-12-08 11:13:23 +0100

    refined and general sets for pdbbind wrapper

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -9,36 +9,46 @@ test_data_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_pdbbind():
+
+    results = {
+        'core': (['1y6r', '10gs'],
+                 [10.11, 6.4]),
+        'refined': (['1ps3', '4x6p', '1y6r', '10gs'],
+                    [2.28, 8.30, 10.11, 6.4]),
+        'general_PL': (['2lyw', '4n7h', '1ps3', '4x6p', '1y6r', '10gs'],
+                       [4.52, 5.48, 2.28, 8.30, 10.11, 6.4]),
+    }
+
     for year in [2007, 2013, 2016]:
         pdbbind_db = pdbbind(home=os.path.join(test_data_dir, 'data', 'pdbbind'),
                              version=year, default_set='core')
 
-        # mock dataset has 4 targets
-        assert_equal(len([i for i in pdbbind_db]), 4)
+        for set_name, (ids, activities) in results.items():
+            if set_name == 'general_PL' and year == 2007:
+                set_name = 'general'
+            pdbbind_db.default_set = set_name
+            assert_equal(pdbbind_db.ids, ids)
+            assert_equal(pdbbind_db.activities, activities)
+
+            for pid in pdbbind_db:
+                assert_is_instance(pid.pocket, oddt.toolkit.Molecule)
+                assert_is_instance(pid.ligand, oddt.toolkit.Molecule)
+                if pid.id == '1ps3':
+                    assert_equal(pid.protein, None)
+                else:
+                    assert_is_instance(pid.protein, oddt.toolkit.Molecule)
 
         # getting by name
         assert_equal(pdbbind_db['10gs'].id, '10gs')
 
         # getting by id
-        assert_equal(pdbbind_db[3].id, '10gs')
-
-        assert_equal(pdbbind_db.ids, ['1ps3', '4x6p', '1y6r', '10gs'])
-        assert_equal(pdbbind_db.activities, [2.28, 8.30, 10.11, 6.4])
+        assert_equal(pdbbind_db[-1].id, '10gs')
 
         # TODO: getting by name error
         # assert_raises(KeyError, pdbbind_db.__getitem__, 'xxxx')
 
         # TODO: getting by id error
         # assert_raises(KeyError, pdbbind_db.__getitem__, 123)
-
-        # test all pockets
-        for pid in pdbbind_db:
-            assert_is_instance(pid.pocket, oddt.toolkit.Molecule)
-            assert_is_instance(pid.ligand, oddt.toolkit.Molecule)
-            if pid.id == '1ps3':
-                assert_equal(pid.protein, None)
-            else:
-                assert_is_instance(pid.protein, oddt.toolkit.Molecule)
 
         pid = pdbbind_db['10gs']
         # get ligand
