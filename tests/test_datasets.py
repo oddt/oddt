268283f5ff769d3d47860fe483dac29818c7d734
marta-sd <martasd@ibb.waw.pl>
2017-12-11 15:11:23 +0100

    rdkit-friendly examples

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -11,12 +11,12 @@ test_data_dir = os.path.dirname(os.path.abspath(__file__))
 def test_pdbbind():
 
     results = {
-        'core': (['1y6r', '10gs'],
-                 [10.11, 6.4]),
-        'refined': (['4up5', '4x6p', '1y6r', '10gs'],
-                    [2.60, 8.30, 10.11, 6.4]),
-        'general_PL': (['4uyh', '4qf8', '4up5', '4x6p', '1y6r', '10gs'],
-                       [6.30, 7.51, 2.60, 8.30, 10.11, 6.4]),
+        'core': (['4yef', '10gs'],
+                 [5.35, 6.4]),
+        'refined': (['1nlp', '1imx', '4yef', '10gs'],
+                    [4.96, 3.52, 5.35, 6.4]),
+        'general_PL': (['1k9q', '1nlo', '1nlp', '1imx', '4yef', '10gs'],
+                       [3.15, 5.47, 4.96, 3.52, 5.35, 6.4]),
     }
 
     for year in [2007, 2013, 2016]:
@@ -35,17 +35,17 @@ def test_pdbbind():
                 assert_greater(len(pid.pocket.atoms), 0)
                 assert_is_instance(pid.ligand, oddt.toolkit.Molecule)
                 assert_greater(len(pid.ligand.atoms), 0)
-                if pid.id == '1y6r':
+                if pid.id == '10gs':
                     assert_equal(pid.protein, None)
                 else:
                     assert_is_instance(pid.protein, oddt.toolkit.Molecule)
                     assert_greater(len(pid.protein.atoms), 0)
 
         # getting by name
-        assert_equal(pdbbind_db['10gs'].id, '10gs')
+        assert_equal(pdbbind_db['1imx'].id, '1imx')
 
         # getting by id
-        assert_equal(pdbbind_db[-1].id, '10gs')
+        assert_equal(pdbbind_db[-3].id, '1imx')
 
         # TODO: getting by name error
         # assert_raises(KeyError, pdbbind_db.__getitem__, 'xxxx')
@@ -53,18 +53,18 @@ def test_pdbbind():
         # TODO: getting by id error
         # assert_raises(KeyError, pdbbind_db.__getitem__, 123)
 
-        pid = pdbbind_db['10gs']
+        pid = pdbbind_db['1imx']
         # get ligand
         ligand = pid.ligand
         ligand.removeh()
-        assert_equal(len(ligand.atoms), 33)
+        assert_equal(len(ligand.atoms), 60)
 
         # get pocket
         pocket = pid.pocket
         pocket.removeh()
-        assert_equal(len(pocket.atoms), 603)
+        assert_equal(len(pocket.atoms), 234)
 
         # protein do exist
         protein = pid.protein
         protein.removeh()
-        assert_equal(len(protein.atoms), 3431)
+        assert_equal(len(protein.atoms), 478)
