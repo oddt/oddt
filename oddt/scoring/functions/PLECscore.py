62301c790e0c8228643d8ffe04ccd96a5c0c813a
Maciek Wójcikowski <maciek@wojcikowski.pl>
2018-02-14 14:04:27 +0100

    Initial PLEC-linear json support

--- oddt/scoring/functions/PLECscore.py
+++ oddt/scoring/functions/PLECscore.py
@@ -2,7 +2,9 @@ from __future__ import print_function
 import sys
 from os.path import dirname, isfile, join as path_join
 from functools import partial
+import json
 
+import numpy as np
 from scipy.stats import pearsonr
 from sklearn.metrics import r2_score
 
@@ -83,46 +85,96 @@ class PLECscore(scorer):
             sparse=True,
         )
 
-    def train(self, home_dir=None, sf_pickle=None, pdbbind_version=2016):
+    def gen_json(self, home_dir=None, pdbbind_version=2016):
+        self.train(home_dir=home_dir)
+        if isinstance(self.model, SGDRegressor):
+            attributes = ['coef_', 'intercept_']
+        elif isinstance(self.model, MLPRegressor):
+            attributes = ['loss_', 'coefs_', 'intercepts_', 'n_iter_',
+                          'n_layers_', 'n_outputs_', 'out_activation_']
+
+        out = {}
+        for attr_name in attributes:
+            attr = getattr(self.model, attr_name)
+            # convert numpy arrays to list for json
+            if isinstance(attr, np.array):
+                attr = attr.tolist()
+            elif (isinstance(attr, (list, tuple)) and
+                  isinstance(attr[0], np.array)):
+                attr = [x.tolist() for x in attr]
+            out[attr_name] = attr
+
+        json_path = path_join(home_dir, 'plecscore_%s_p%i_l%i_s%i_pdbbind%i.json' %
+                              (self.version, self.depth_protein,
+                               self.depth_ligand, self.size, pdbbind_version))
+
+        with open(json_path, 'w') as json_f:
+            json.dump(out, json_f, indent=2)
+        return json_path
+
+    def train(self, home_dir=None, sf_pickle=None, pdbbind_version=2016,
+              ignore_json=False):
         if not home_dir:
             home_dir = path_join(dirname(__file__), 'PLECscore')
         desc_path = path_join(home_dir, 'plecscore_descs_p%i_l%i_s%i.csv' %
                               (self.depth_protein, self.depth_ligand, self.size))
 
-        super(PLECscore, self)._load_pdbbind_desc(
-            desc_path,
-            train_set=('general', 'refined'),
-            pdbbind_version=pdbbind_version)
+        json_path = path_join(
+            home_dir, 'plecscore_%s_p%i_l%i_s%i_pdbbind%i.json' %
+            (self.version, self.depth_protein,
+             self.depth_ligand, self.size, pdbbind_version))
 
-        # FIXME: RF does not like CSR matrix in OOB pred for older sklearn
-        if self.version == 'rf':
-            self.model.fit(self.train_descs.toarray(), self.train_target)
+        if (self.version in ['linear'] and  # TODO: support other models
+                isfile(json_path) and
+                not ignore_json):
+            print('Loading pretrained PLECscore %s with depths P%i L%i on '
+                  'PDBBind v%i'
+                  % (self.version, self.depth_protein, self.depth_ligand,
+                     pdbbind_version), file=sys.stderr)
+            with open(json_path) as json_f:
+                json_data = json.load(json_f)
+            for k, v in json_data.items():
+                if isinstance(v, list):
+                    if isinstance(v[0], list):
+                        v = [np.array(x) for x in v]
+                    else:
+                        v = np.array(v)
+                setattr(self.model, k, v)
         else:
-            self.model.fit(self.train_descs, self.train_target)
+            super(PLECscore, self)._load_pdbbind_desc(
+                desc_path,
+                train_set=('general', 'refined'),
+                pdbbind_version=pdbbind_version)
 
-        print('Training PLECscore %s with depths P%i L%i on PDBBind v%i'
-              % (self.version, self.depth_protein, self.depth_ligand,
-                 pdbbind_version), file=sys.stderr)
+            print('Training PLECscore %s with depths P%i L%i on PDBBind v%i'
+                  % (self.version, self.depth_protein, self.depth_ligand,
+                     pdbbind_version), file=sys.stderr)
 
-        sets = [
-            ('Test', self.model.predict(self.test_descs), self.test_target),
-            ('Train', self.model.predict(self.train_descs), self.train_target)]
-        if self.version == 'rf':
-            sets.append(('OOB', self.model.oob_prediction_, self.train_target))
+            # FIXME: RF does not like CSR matrix in OOB pred for older sklearn
+            if self.version == 'rf':
+                self.model.fit(self.train_descs.toarray(), self.train_target)
+            else:
+                self.model.fit(self.train_descs, self.train_target)
 
-        for name, pred, target in sets:
-            print('%s set:' % name,
-                  'R2_score: %.4f' % r2_score(target, pred),
-                  'Rp: %.4f' % pearsonr(target, pred)[0],
-                  'RMSE: %.4f' % rmse(target, pred),
-                  sep='\t', file=sys.stderr)
+            sets = [
+                ('Test', self.model.predict(self.test_descs), self.test_target),
+                ('Train', self.model.predict(self.train_descs), self.train_target)]
+            if self.version == 'rf':
+                sets.append(('OOB', self.model.oob_prediction_, self.train_target))
 
-        if sf_pickle is None:
-            return self.save('PLEC%s_p%i_l%i_pdbbind%i_s%i.pickle'
-                             % (self.version, self.depth_protein,
-                                self.depth_ligand, pdbbind_version, self.size))
-        else:
-            return self.save(sf_pickle)
+            for name, pred, target in sets:
+                print('%s set:' % name,
+                      'R2_score: %.4f' % r2_score(target, pred),
+                      'Rp: %.4f' % pearsonr(target, pred)[0],
+                      'RMSE: %.4f' % rmse(target, pred),
+                      sep='\t', file=sys.stderr)
+
+            if sf_pickle is None:
+                return self.save('PLEC%s_p%i_l%i_pdbbind%i_s%i.pickle'
+                                 % (self.version, self.depth_protein,
+                                    self.depth_ligand, pdbbind_version, self.size))
+            else:
+                return self.save(sf_pickle)
 
     @classmethod
     def load(self, filename=None, version='linear', pdbbind_version=2016):
