b5927dd46c016be1ccff5e396b818c80ffaf509e
Michał Kukiełka <michal.kukielka5@gmail.com>
2019-05-21 11:09:32 +0200

    Implemented ri_score and b-factor.

--- oddt/fingerprints.py
+++ oddt/fingerprints.py
@@ -8,6 +8,7 @@ from six.moves import zip_longest
 from itertools import chain
 from collections import OrderedDict
 import numpy as np
+import numexpr as ne
 from scipy.sparse import csr_matrix, isspmatrix_csr
 import oddt
 from oddt.utils import is_openbabel_molecule
@@ -17,6 +18,7 @@ from oddt.interactions import (pi_stacking,
                                hydrophobic_contacts,
                                acceptor_metal,
                                close_contacts)
+from oddt.spatial import rmsd
 
 
 __all__ = ['InteractionFingerprint',
@@ -864,3 +866,84 @@ def tanimoto(a, b, sparse=False):
         if denominator > 0:
             return a_b / denominator
     return 0.
+
+
+def distance(a, b):
+    """Numexpr powered distance that is float32 friendly (memory efficient)"""
+    a = a[..., np.newaxis, :]
+    return np.sqrt(ne.evaluate('sum((a - b)**2, axis=2)'))
+
+
+def b_factor(ligand, protein, cutoff=10., correlation_type='exponential',
+             tau=3., k=40, v=40):
+    """Flexibility-Rigity Index, based on  10.1021/acs.jcim.7b00226"""
+    # dicts with heavy atoms
+    ligand_atoms = ligand.atom_dict[ligand.atom_dict['atomicnum'] > 1]
+    protein_atoms = protein.atom_dict[protein.atom_dict['atomicnum'] > 1]
+    complex_atoms = np.hstack((protein_atoms, ligand_atoms))
+
+    # distance matrix for heavy atoms, casted to float32 for RAM efficiency
+    distance_complex = distance(complex_atoms['coords'],
+                                complex_atoms['coords']).astype(np.float32)
+    # Reuse distances to save time and memory
+    n_prot = len(protein_atoms)
+    distance_protein = distance_complex[:n_prot, :n_prot]
+    assert distance_complex.shape == (len(complex_atoms), len(complex_atoms))
+    assert distance_protein.shape == (n_prot, n_prot)
+
+    # weights, which are set to 1
+    w_protein = 1.  # / (len(protein_atoms) ** 2)
+    w_complex = 1.  # / ((len(ligand_atoms) + len(protein_atoms)) * len(protein_atoms))
+
+    b_factor_protein = 1. / correlation(distance_protein,
+                                        w_protein,
+                                        protein_atoms['radius'],
+                                        correlation_type=correlation_type,
+                                        k=k,
+                                        v=v)
+    b_factor_complex = 1. / correlation(distance_complex,
+                                        w_complex,
+                                        complex_atoms['radius'],
+                                        correlation_type=correlation_type,
+                                        k=k,
+                                        v=v)
+
+    delta_bfactor = (b_factor_complex - b_factor_protein) / b_factor_protein
+
+    return delta_bfactor
+
+
+def ri_score(ligand, protein, cutoff=10., correlation_type='exponential',
+             tau=3.,  k=40, v=40):
+    # dicts with heavy atoms
+    ligand_atoms = ligand.atom_dict[ligand.atom_dict['atomicnum'] > 1]
+    protein_atoms = protein.atom_dict[protein.atom_dict['atomicnum'] > 1]
+    distance_complex = distance(protein_atoms['coords'],
+                                ligand_atoms['coords']).astype(np.float32)
+    assert distance_complex.shape == (len(protein_atoms), len(ligand_atoms))
+    weight = 1.
+    return correlation(distance_complex,
+                       weight,
+                       protein_atoms['radius'],
+                       ligand_atoms['radius'],
+                       correlation_type=correlation_type,
+                       k=k,
+                       v=v)
+
+
+def correlation(distance, weight, radius, radius_secondary=None,
+                correlation_type='exponential', tau=3., k=40, v=40):
+    assert len(distance) == len(radius)
+    # the Numexpr versions are 2x faster and use less memory
+    # eta is now calculated on the fly in the numexpr to save memory
+    # eta = radius[:, np.newaxis] * radius_secondary * tau
+    if radius_secondary is None:
+        radius_secondary = radius
+    radius = radius[:, np.newaxis]  # numexpr does not hadle this well
+    if correlation_type == 'exponential':
+        return float(ne.evaluate('sum(weight * exp(-((distance / (radius * radius_secondary * tau)) ** k)))'))
+    elif correlation_type == 'lorentz':
+        return float(ne.evaluate('sum(weight * 1. / (1. + ((distance / (radius * radius_secondary * tau)) ** v)))'))
+    else:
+        raise ValueError('"%s" is unsupported correlation function type. '
+                         'Use one of: ["exponential", "lorentz"]/' % correlation_type)
