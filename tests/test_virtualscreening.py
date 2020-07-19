1ae5f189a1f3c14f7361ee3f4d0e48ff0908cb8c
marta-sd <m.m.stepniewska@gmail.com>
2020-07-19 12:34:33 +0200

    get back to 0.8 cutoff in vs.similarity

--- tests/test_virtualscreening.py
+++ tests/test_virtualscreening.py
@@ -175,19 +175,19 @@ def test_vs_similarity():
 
     vs = virtualscreening(n_cpu=1)
     vs.load_ligands('sdf', xiap_actives_docked)
-    vs.similarity('ifp', cutoff=0.9, query=ref_mol, protein=receptor)
+    vs.similarity('ifp', cutoff=0.8, query=ref_mol, protein=receptor)
     if oddt.toolkit.backend == 'ob':
-        assert len(list(vs.fetch())) == 11
+        assert len(list(vs.fetch())) == 33
     else:
-        assert len(list(vs.fetch())) == 10
+        assert len(list(vs.fetch())) == 22
 
     vs = virtualscreening(n_cpu=1)
     vs.load_ligands('sdf', xiap_actives_docked)
-    vs.similarity('sifp', cutoff=0.9, query=ref_mol, protein=receptor)
+    vs.similarity('sifp', cutoff=0.8, query=ref_mol, protein=receptor)
     if oddt.toolkit.backend == 'ob':
-        assert len(list(vs.fetch())) == 11
+        assert len(list(vs.fetch())) == 33
     else:
-        assert len(list(vs.fetch())) == 10
+        assert len(list(vs.fetch())) == 22
 
     # test wrong method error
     with pytest.raises(ValueError):
